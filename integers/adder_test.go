950347c9cf144c30cc0317cd9f6c3391f284a62d
Andrew Mititi <andymwebi@gmail.com>
2025-03-07 12:03:31 +0300

    integer: with both test and example

---
+++ integers/adder_test.go
@@ -0,0 +1,23 @@
+package integers
+
+import (
+	"fmt"
+	"testing"
+)
+
+
+func TestAdder(t *testing.T){
+	sum := Add(2, 2)
+	expected := 4
+
+	if sum != expected {
+		t.Errorf("expected '%d' got '%d'", expected, sum)
+	}
+}
+
+
+func ExampleAdd(){
+	sum := Add(1,3)
+	fmt.Println(sum)
+	// Output: 4
+}
